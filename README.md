476054b192867b73e5f436746b2e3e7a5a27ce18
grimjoke <konsmits1@gmail.com>
2022-01-25 00:14:58 +0200

    add submodule pull down

---
+++ README.md
@@ -0,0 +1,6 @@
+# configure and build with CMake using command line
+
+## Linux command line
+        ./configure.sh
+        ./build.sh
+        ./run.sh
--- main.cpp
+++ main.cpp
@@ -1,10 +1,41 @@
 #include <iostream>
 #include <adder.h>
-
+#include <GLFW/glfw3.h>
 
 int main(){
     std::cout << "Hello, World !" << std::endl;
 
     std::cout << add(72.1, 73.8) << std::endl;
+
+    GLFWwindow *window;
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        exit( EXIT_FAILURE );
+    }
+
+    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+    if (!window)
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+
+        // Main loop
+    while( !glfwWindowShouldClose(window) )
+    {
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
+
+    // Exit program
+    exit( EXIT_SUCCESS );
+
     return 0;
 }
